5d507b1d5c8141493a0c5146e9825fb0840a0d2d
Dmitriy <spirin2205@yandex.ru>
2019-07-23 10:27:38 +0300

    gallery and banks

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -4,7 +4,69 @@ document.addEventListener("DOMContentLoaded", function() {
     setAppartmentSlider();
     setSliderBtns();
     openMobileMenu();
+    masonryGalleryMaker();
+    scrollAmimationInit();
+    appsFilterBtnEvent();
 });
+
+function scrollAmimationInit(){
+    AOS.init({
+        // Global settings:
+        disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+        initClassName: 'aos-init', // class applied after initialization
+        animatedClassName: 'aos-animate', // class applied on animation
+        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+        disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+        
+      
+        // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+        offset: 120, // offset (in px) from the original trigger point
+        delay: 0, // values from 0 to 3000, with step 50ms
+        duration: 400, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+        once: false, // whether animation should happen only once - while scrolling down
+        mirror: false, // whether elements should animate out while scrolling past them
+        anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+      
+      });
+}
+
+function onePageScrollInit(){
+    $.scrollify({
+        section : ".page-scroll",
+        sectionName : "section-name",
+        interstitialSection : ".page-scroll--half",
+        easing: "easeOutExpo",
+        scrollSpeed: 1100,
+        offset : 0,
+        scrollbars: true,
+        standardScrollElements: "",
+        setHeights: true,
+        overflowScroll: true,
+        updateHash: true,
+        touchScroll:true,
+        scrollBar: false,
+        before:function() {},
+        after:function() {},
+        afterResize:function() {},
+        afterRender:function() {}
+      });
+}
+
+function masonryGalleryMaker(){
+    var $grid = $('.grid-gallery').masonry({
+        itemSelector: '.grid-gallery-item',
+        percentPosition: true,
+        columnWidth: '.grid-sizer',
+      });
+      // layout Masonry after each image loads
+      $grid.imagesLoaded().progress( function() {
+        $grid.masonry();
+      });  
+}
 
 function openMobileMenu(){
     $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
@@ -40,19 +102,23 @@ function setAppartmentSlider(){
     });
 }
 
-
-
-function setSliderBtns(){
-    
+function appsFilterBtnEvent(){
     $('.appartments__filter-item').click(function(){
         var aim = this.getAttribute('data-filter-slider');
         var slides = document.querySelectorAll('.appartments__slider .owl-item .slide-item');
-        for (let index = 0; index < slides.length; index++) {
+        var counter = 0;
+        for (let index = 0; index < slides.length && counter <= 0; index++) {
             if(slides[index].classList.contains(aim)){
-                $('.owl-carousel').trigger('to.owl.carousel', index)
+                counter++;
+                $('.owl-carousel').trigger('to.owl.carousel', index + 1 );
             }
         }
     });
+}
+
+function setSliderBtns(){
+    
+
     $('.appartments__filter-item').removeClass('active');
     var $slideActive = $('.appartments__slider .owl-item.active.center .slide-item');
     if($slideActive.hasClass('double')){
